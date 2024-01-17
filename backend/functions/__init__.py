57e5a31b8fc42205b6fdb9a7687d85ac1e596c38
kinxyos <kinjalkt3@gmail.com>
2024-01-17 15:10:53 +0530

    basic functionlities added

--- backend/functions/__init__.py
+++ backend/functions/__init__.py
@@ -3,18 +3,8 @@ import pickle
 import requests
 
 # GLOBAL VARIABLES
-movies = pickle.load(open('././data/artifacts/movie_list.pkl','rb'))
-similarity = pickle.load(open('././data/artifacts/similarity.pkl','rb'))
-
-# FUNCTIONS
-def save_search(movie):
-    with open("././data/archive/history.txt","r") as check:
-        content = check.read()
-        if movie in content:
-            pass
-        else:
-            with open('././data/archive/history.txt','a') as file:
-                file.write(f"{movie}\n")
+movies = pickle.load(open('././ml/output/movie_list.pkl','rb'))
+similarity = pickle.load(open('././ml/output/similarity.pkl','rb'))
 
 def fetch_poster(movie_id):
     url = "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(movie_id)
@@ -25,14 +15,20 @@ def fetch_poster(movie_id):
     return full_path
 
 def recommend(movie):
-    index = movies[movies['title'] == movie].index[0]
-    distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
-    recommended_movie_names = []
-    recommended_movie_posters = []
-    for i in distances[1:6]:
-        # fetch the movie poster
-        movie_id = movies.iloc[i[0]].movie_id
-        recommended_movie_posters.append(fetch_poster(movie_id))
-        recommended_movie_names.append(movies.iloc[i[0]].title)
-
-    return recommended_movie_names,recommended_movie_posters
+    print(movie)
+    # index = movies[movies['title'] == movie].index[0]
+    movie_df = movies[movies['title'] == movie]
+    if not movie_df.empty:
+        index = movie_df.index[0]
+        distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
+        recommended_movie_names = []
+        recommended_movie_posters = []
+        for i in distances[1:6]:
+            # fetch the movie poster
+            movie_id = movies.iloc[i[0]].movie_id
+            recommended_movie_posters.append(fetch_poster(movie_id))
+            recommended_movie_names.append(movies.iloc[i[0]].title)
+        return recommended_movie_names,recommended_movie_posters
+    else:
+        print(f"No movie found with title {movie}")
+        return
---
+++ db/models.py
@@ -0,0 +1,8 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(200))
+    complete = db.Column(db.Boolean)
