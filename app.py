64975d3d3b562345c393cdddeb5d62ca95bc2cfd
kinxyos <kinjalkt3@gmail.com>
2024-01-17 22:15:24 +0530

    database integration complete

--- app.py
+++ app.py
@@ -1,64 +1,50 @@
 # IMPORTS
-from flask import Flask, redirect, render_template, request
-from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+import random
+from flask import Flask, render_template, request
+from backend.functions import recommend
+from backend.models import db
+from backend.models import Records
 
-from backend.functions import *
-
-app = Flask(__name__, template_folder="templates")
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///todo.db"
+# APP CONFIG
+app = Flask(__name__, template_folder="pages")
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///recomsys.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
+db.init_app(app)
 
-class Todo(db.Model):
-    sno = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    desc = db.Column(db.String(500), nullable=False)
-    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+with app.app_context():
+    db.create_all()
 
-    def __repre__(self) -> str:
-        return f"{self.sno}-> {self.title}"
-    
 
+# ROUTES
 @app.route('/', methods=['GET','POST'])
 def Home():
-    # todo = Todo(title="Listen Close!", desc="Start investing in Stocks!")
-    # db.session.add(todo)
-    # db.session.commit(todo)
+
+
     if request.method == 'POST':
+        
+        # GETTING RECOMMENDATION
         film = request.form['film']
         movie, poster = recommend(film)
-        for i in range(0,5):
-            print(movie[i])
+        suggested = random.randint(0,5)
+        
+        # ADDING TO RECORDS 
+        db.session.add(Records(title=film, recom_mov=movie[suggested], recom_thum=poster[suggested])) #the error is probably because there are no records yet, "what about the exception msg?" yeah it's bullshit. people who use python aren't really good at coding you know.
+        db.session.commit()
+        
         return render_template("index.html", posters=poster)
+    
+
     elif request.method == 'GET':
         movies_searched = []
-        search_history = []
-        with open("history.txt","r") as search_records:
-            history = search_records.readlines()
-            for movies in history:
-                tmp_list = movies.split("\n")
-                search_history.insert(0, tmp_list[0])            
-            
-            for i in range(5):
-                movie, poster = recommend(search_history[i])
-                movies_searched.append({'name': movie[i], 'thumbnail': poster[i]})
 
-        print(movies_searched)
+        search_history = Records.query.order_by(Records.sno.desc()).limit(5).all()
+        
+        for record in search_history:
+            movie, poster = recommend(record.title)
+            movies_searched.append({'name': movie[0], 'thumbnail': poster[0]})
+
         return render_template("index.html", history=movies_searched)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
-""" 
-     with open("history.txt","r") as check:
-            content = check.read()
-            if film in content:
-                print("film already present in records")
-                pass
-            else:
-                print(f"adding {film}")
-                with open('history.txt','a') as file:
-                    file.write(f"{film}\n")
-"""
