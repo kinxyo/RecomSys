681fc3b9337a1aba6d72bb6a65cfec6405e2f8f4
kinxyos <kinjalkt3@gmail.com>
2024-01-22 20:46:13 +0530

    added search stage, improved ui, removed auth and fixed data redundancy in search records

--- static/js/script.js
+++ static/js/script.js
@@ -1,14 +1,16 @@
 export function RecommendMovies() {
-    data = document.querySelector('input[name="film"]').value
+    let data = document.querySelector('input[name="film"]').value;
     fetch('/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ film: data }),
+        body: JSON.stringify(data),
     })
-    .then(response => response.json())
-    .then(data => console.log(data))
+    .then()
+    .then()
     .catch((error) => console.error('Error:', error));
 }
 
+// Attach the RecommendMovies function as a click event handler
+document.getElementById('recommend-button').addEventListener('click', RecommendMovies);
