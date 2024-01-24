67706de6f17e7457032f4110c0c166c767fe8af6
kinxyos <kinjalkt3@gmail.com>
2024-01-24 13:34:06 +0530

    no comments

--- app.py
+++ app.py
@@ -8,8 +8,9 @@ from backend.models import Records, db
 app = Flask(__name__, template_folder="pages")
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///recomsys.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db.init_app(app)
 
+# DATABASE
+db.init_app(app)
 with app.app_context():
     db.create_all()
 
